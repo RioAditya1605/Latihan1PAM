4a6fa2889b9179b52a8663a51943878c074729a7
RioAditya1605 <rioaditya1605@gmail.com>
2024-03-08 22:23:57 +0700

    commit

--- app/src/main/java/com/example/tugas1pam/MainActivity.kt
+++ app/src/main/java/com/example/tugas1pam/MainActivity.kt
@@ -2,41 +2,34 @@ package com.example.tugas1pam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.btn_kurang
 import kotlinx.android.synthetic.main.activity_main.btn_tambah
-import kotlinx.android.synthetic.main.activity_main.et_angka1
-import kotlinx.android.synthetic.main.activity_main.et_angka2
 import kotlinx.android.synthetic.main.activity_main.tv_hasil
 
 class MainActivity : AppCompatActivity() {
+    private var hitung = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Mengatur listener untuk tombol "Tambah"
-        btn_tambah.setOnClickListener {
-            // Mengambil angka dari input pengguna dan mengonversinya ke dalam bentuk string
-            val nilai_angka1 = et_angka1.text.toString().toInt()
-            val nilai_angka2 = et_angka2.text.toString().toInt()
+        val textViewCounter = findViewById<TextView>(R.id.tv_hasil)
 
-            // Menjumlahkan dua angka
-            val hasil = nilai_angka1 + nilai_angka2
+        textViewCounter.text = hitung.toString()
 
-            // Menampilkan hasil penjumlahan di TextView
-            tv_hasil.text = hasil.toString()
+        val buttontambah = findViewById<Button>(R.id.btn_tambah)
+        val buttonkurang = findViewById<Button>(R.id.btn_kurang)
+
+        buttontambah.setOnClickListener {
+            hitung++
+            textViewCounter.text = hitung.toString()
         }
 
-        // Mengatur listener untuk tombol "Kurang"
-        btn_kurang.setOnClickListener {
-            // Mengambil angka dari input pengguna dan mengonversinya ke dalam bentuk string
-            val nilai_angka1 = et_angka1.text.toString().toInt()
-            val nilai_angka2 = et_angka2.text.toString().toInt()
-
-            // Pengurangan dua angka
-            val hasil = nilai_angka1 - nilai_angka2
-
-            // Menampilkan hasil Pengurangan di TextView
-            tv_hasil.text = hasil.toString()
+        buttonkurang.setOnClickListener {
+            hitung--
+            textViewCounter.text = hitung.toString()
         }
+
     }
 }
